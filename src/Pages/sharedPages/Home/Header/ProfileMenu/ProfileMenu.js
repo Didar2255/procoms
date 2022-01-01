7f926885a1bb43088ea40c51aaf3d4fcb39880e3
coder71 <kamrunnaher877@gmail.com>
2022-01-01 22:30:16 +0600

    basic header added

---
+++ src/Pages/sharedPages/Home/Header/ProfileMenu/ProfileMenu.js
@@ -0,0 +1,59 @@
+import { Avatar, Button } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Box } from '@mui/system';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import avatar from '../../../../../assets/avatar.png';
+import Navigation from '../../../../../components/Navigation';
+
+const ProflieMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogOut = () => {
+    // will logout from here
+    navigate('/');
+  };
+
+  return (
+    <>
+      <Box onClick={handleClick}>
+        <Avatar
+          sx={{ ml: 'auto', width: 60, height: 60, cursor: 'pointer' }}
+          src={avatar}
+          alt="profile picture"
+        />
+      </Box>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Navigation name="Dashboard" path="/dashboard" />
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Button variant="outlined" onClick={handleLogOut}>
+            Logout
+          </Button>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default ProflieMenu;
