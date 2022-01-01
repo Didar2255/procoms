22303bb1f93592c40d15553e5b4b599720968b40
coder71 <kamrunnaher877@gmail.com>
2022-01-01 21:58:18 +0600

    simple routing setup added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../../../real_project/devmemflash/src/pages/Home/Home';
+import Pricing from '../../../real_project/devmemflash/src/pages/Pricing/Pricing';
+import Footer from '../../../real_project/devmemflash/src/pages/shared/Footer/Footer';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Header from './Pages/sharedPages/Home/Header/Header';
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
