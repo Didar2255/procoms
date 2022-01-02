4a3e878e01ebf8465cf2a1b3eb9f3a597d757cb0
coder71 <kamrunnaher877@gmail.com>
2022-01-02 11:02:06 +0600

    user and admin dashboard pages added

--- src/Pages/sharedPages/Home/Home.js
+++ src/Pages/sharedPages/Home/Home.js
@@ -1,11 +1,12 @@
+import { Box } from '@mui/material';
 import React from 'react';
 import Banner from './Banner/Banner';
 
 const Home = () => {
   return (
-    <div>
+    <Box>
       <Banner></Banner>
-    </div>
+    </Box>
   );
 };
 
