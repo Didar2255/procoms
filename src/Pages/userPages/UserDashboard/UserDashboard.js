05a2dad9f75e4f8c9a3b70cbfa0de3ca235a839a
coder71 <kamrunnaher877@gmail.com>
2022-01-03 22:41:20 +0600

    initial user dashboard added

--- src/Pages/userPages/UserDashboard/UserDashboard.js
+++ src/Pages/userPages/UserDashboard/UserDashboard.js
@@ -1,11 +1,27 @@
 import { Box } from '@mui/system';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import DashboardHeader from '../../../components/DashboardHeader';
+
+const links = [
+  {
+    name: 'Pay',
+    path: '/user/pay',
+  },
+  {
+    name: 'MyOrders',
+    path: '/user/myOrders',
+  },
+  {
+    name: 'Add Review',
+    path: '/user/addReview',
+  },
+];
 
 const UserDashboard = () => {
   return (
-    <Box>
-      <Box>user Header or sidebar will be here</Box>
+    <Box container style={{ minHeight: 'calc(100vh - 270px)' }} sx={{ mt: 3 }}>
+      <DashboardHeader links={links} />
       <Outlet />
     </Box>
   );
